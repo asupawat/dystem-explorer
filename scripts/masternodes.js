a1dbe33b6f7004fab59673ffccc2722166f47c88
Danystem <dan@dystem.io>
2018-05-11 10:49:13 +0000

    Fix masternode syncing and ui for variation in RPC listmasternodes

--- scripts/masternodes.js
+++ scripts/masternodes.js
@@ -13,6 +13,7 @@ dbString = dbString + '@' + settings.dbsettings.address;
 dbString = dbString + ':' + settings.dbsettings.port;
 dbString = dbString + '/' + settings.dbsettings.database;
 
+
 var MasternodesSchema = new mongoose.Schema({
     hash: {type: String, default: ""},
     status: {type: String, default: ""},
@@ -32,22 +33,36 @@ function exit() {
 
 function Action() {
     var req = request({
-        uri: 'http://127.0.0.1:' + settings.port + '/api/masternodelist?mode=full',
+        uri: 'http://127.0.0.1:' + settings.port + '/api/listmasternodes',
         json: true
     }, function (error, response, body) {
-        var mn_hash = Object.keys(body);
+    
+console.log(body)
+    var mn_hash = Object.keys(body);
 	MyModel.remove({}, function(err) { 
 	   console.log('collection removed')
 	});
+	
+	
+	
         lib.syncLoop(mn_hash.length, function (loop) {
             var mn = mn_hash[loop.iteration()];
-            var mn_data = body[mn].split(/\s+/);
+            
+			/*
+			var mn_data = body[mn].split(/\s+/);
             var mn_status = mn_data[1];
             var mn_pubkey = mn_data[3];
             var mn_address = mn_data[4];
             var mn_lastseen = mn_data[5];
             var mn_activesec = mn_data[6];
-
+			*/
+			
+			mn_status=body[mn].status;
+			mn_pubkey=body[mn].addr;
+			mn_address ='';
+			mn_lastseen=body[mn].lastseen;
+			mn_activesec=body[mn].activetime;
+			
             var newmn = MyModel.findOneAndUpdate({hash: mn},{
 
                 status: mn_status,
